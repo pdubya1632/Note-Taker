d7865a7651085cfc1b1f2e202965c175a1113ed4
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-21 12:24:34 -0700

    add more placeholder notes to db

--- server.js
+++ server.js
@@ -5,17 +5,35 @@ const path = require('path');
 const fs = require('fs');
 const util = require('util');
 
+// create app and assign port
 const app = express();
 const PORT = 3001;
 
+// middleware
+app.use(express.json());
 app.use(express.static('public'));
 
-// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+// GET request for api route
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+// POST request for api route
+
+// DELETE request for api route
+
+// GET request routes : wildcard fallback
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
+// GET request routes for index and notes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+// Port started message
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
