bce5b2a22a3ac5ef9dbb0a1e1dae0d87dbcf075f
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-21 11:44:35 -0700

    connect notes route

--- server.js
+++ server.js
@@ -4,3 +4,18 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
