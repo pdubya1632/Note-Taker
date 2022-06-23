d292ae0b4a729a814daf9367a898720b933351fb
Patrick Richardson Wright <wrightmight@gmail.com>
2022-06-22 19:53:07 -0700

    install dotenv

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { readFile, writeFile } = require("fs").promises;
 
 // create app and assign port
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 // Express middleware
 app.use(express.static('public'));
